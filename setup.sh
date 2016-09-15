ddf8c906d49dfb2e52512ef449ca137dec96d6e3
drencken <dieter@Zen.localdomain>
2016-09-14 18:57:02 -0700

    Fix

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 # update the list of packages that can be installed
 apt-get update
 # install git etc.
-apt-get install y git
-apt-get install y openvpn
+apt-get install -y git
+apt-get install -y openvpn
 # Clone the script and execute it
 git clone https://github.com/jprochazka/adsb-receiver.git
 cd ~/adsb-receiver
